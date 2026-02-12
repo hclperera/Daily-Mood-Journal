007ad4cd09da6744cd0d0043039279c556a21fd4
Abishuuu <sriabisan06@gmail.com>
2026-02-12 21:53:28 +0530

    Create database and add users table schema

--- app/src/main/java/com/example/daily_mood_journal/database/DatabaseHelper.java
+++ app/src/main/java/com/example/daily_mood_journal/database/DatabaseHelper.java
@@ -1,4 +1,37 @@
 package com.example.daily_mood_journal.database;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "moodjournal.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_USERS = "users";
+    public static final String COLUMN_USER_ID = "id";
+    public static final String COLUMN_USER_NAME = "name";
+    public static final String COLUMN_USER_EMAIL = "email";
+    public static final String COLUMN_USER_PASSWORD = "password";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_USERS_TABLE = "CREATE TABLE " + TABLE_USERS + "("
+                + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_USER_NAME + " TEXT,"
+                + COLUMN_USER_EMAIL + " TEXT UNIQUE,"
+                + COLUMN_USER_PASSWORD + " TEXT" + ")";
+        db.execSQL(CREATE_USERS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        onCreate(db);
+    }
 }
