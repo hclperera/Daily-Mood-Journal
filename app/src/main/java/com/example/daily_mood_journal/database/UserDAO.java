a2b507f3af04e1d5fdd33a2a11550a341bd1febf
Abishuuu <sriabisan06@gmail.com>
2026-02-13 11:55:29 +0530

    Implement user registration in UserDAO with password hashing

--- app/src/main/java/com/example/daily_mood_journal/database/UserDAO.java
+++ app/src/main/java/com/example/daily_mood_journal/database/UserDAO.java
@@ -1,4 +1,29 @@
 package com.example.daily_mood_journal.database;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.daily_mood_journal.utils.PasswordUtils;
+
 public class UserDAO {
+
+    private DatabaseHelper dbHelper;
+
+    public UserDAO(Context context) {
+        dbHelper = new DatabaseHelper(context);
+    }
+
+    // Register user
+    public boolean registerUser(String name, String email, String password) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.COLUMN_USER_NAME, name);
+        values.put(DatabaseHelper.COLUMN_USER_EMAIL, email);
+        values.put(DatabaseHelper.COLUMN_USER_PASSWORD, PasswordUtils.hashPassword(password));
+
+        long result = db.insert(DatabaseHelper.TABLE_USERS, null, values);
+        db.close();
+        return result != -1;
+    }
 }
