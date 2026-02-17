290688c2a52c7fb8c0cd12db6165766e69afa528
Abishuuu <sriabisan06@gmail.com>
2026-02-17 22:51:43 +0530

    Add SplashActivity to redirect users based on session state

--- app/src/main/java/com/example/daily_mood_journal/activities/SplashActivity.java
+++ app/src/main/java/com/example/daily_mood_journal/activities/SplashActivity.java
@@ -1,26 +1,29 @@
 package com.example.daily_mood_journal.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.daily_mood_journal.R;
+import com.example.daily_mood_journal.utils.SessionManager;
 
 public class SplashActivity extends AppCompatActivity {
+
+    SessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        session = new SessionManager(this);
+
+        if (session.getUserEmail() != null) {
+            // Already logged in
+            startActivity(new Intent(SplashActivity.this, DashboardActivity.class));
+        } else {
+            // Go to login
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        }
+        finish();
     }
 }
