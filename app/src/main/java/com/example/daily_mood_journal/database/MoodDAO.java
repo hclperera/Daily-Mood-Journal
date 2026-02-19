ee378914c3ba4f1db3ad3172ea98e8aa70ecadad
Abishuuu <sriabisan06@gmail.com>
2026-02-19 22:08:18 +0530

    Add MoodDAO class with DatabaseHelper initialization

--- app/src/main/java/com/example/daily_mood_journal/database/MoodDAO.java
+++ app/src/main/java/com/example/daily_mood_journal/database/MoodDAO.java
@@ -1,4 +1,11 @@
 package com.example.daily_mood_journal.database;
 
+import android.content.Context;
 public class MoodDAO {
+
+    private DatabaseHelper dbHelper;
+
+    public MoodDAO(Context context) {
+        dbHelper = new DatabaseHelper(context);
+    }
 }
